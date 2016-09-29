49248061a58ee2e88bd3f980bcd5be23b8f02d2c
71619997a <gmarks@stuy.edu>
2016-09-29 17:20:16 -0400

    Update to reflect assignment

--- login.py
+++ login.py
@@ -8,17 +8,18 @@ def loginpage():
     return render_template('home.html')
 
 
-@app.route('/authenticate', methods=['GET','POST'])
+@app.route('/authenticate', methods=['GET', 'POST'])
 def authenticate():
     try:
         user = request.form['user']
         password = request.form['pass']
+        trickery = False
     except:
-        return render_template('hax.html')
-    if user == 'admin' and password == 'alpine':
-        return render_template('success.html')
-    else:
-        return render_template('failure.html')
+        user = 0
+        password = 0
+        trickery = True
+    login_good = user == 'admin' and password == 'alpine'
+    return render_template('auth-response.html', trickery=trickery, login_good=login_good)
 
 if __name__ == '__main__':
     app.debug = True
